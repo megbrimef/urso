4fa32c9c4d68f74f266c10379ef916d71ec23090
Денис Хафизов <mrwipe1234@gmail.com>
2022-07-05 21:59:55 +0300

    [config] comment fix

--- src/js/config/main.js
+++ src/js/config/main.js
@@ -1,6 +1,6 @@
 let ConfigMain = {
     title: 'Urso', //game title
-    appVersion: 0, //app version to load valid resources
+    appVersion: 0, //app version, also used as anticache  "appVersion=${appVersion}" when not 0
     mode: "development", // development/production/testing
     extendingChain: ['Urso.Core'], //chain that will be set as Urso.Game
     defaultScene: 'play', //default scene to display
