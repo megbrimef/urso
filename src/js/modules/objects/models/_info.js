7489a0f4eebccc26bb8e27a2a1b6c878570fca03
SkyLaga <89088654+SkyLaga@users.noreply.github.com>
2022-08-16 10:18:40 +0300

    Feature/0.4/drag container (#69)
    [modulesObjectsModelsDragcontainer] dragContainer implemented

--- src/js/modules/objects/models/_info.js
+++ src/js/modules/objects/models/_info.js
@@ -7,6 +7,7 @@ Urso.Core.Modules.Objects.Models = {
     Collection: require('./collection.js'),
     Component: require('./component.js'),
     Container: require('./container.js'),
+    DragContainer: require('./dragContainer.js'),
     Emitter: require('./emitter.js'),
     EmitterFx: require('./emitterFx.js'),
     Graphics: require('./graphics.js'),
