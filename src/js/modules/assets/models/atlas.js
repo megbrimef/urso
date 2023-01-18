9946d05441f63ae478d798a4fb901048f103980d
SkyLaga <89088654+SkyLaga@users.noreply.github.com>
2023-01-18 19:16:04 +0700

    [ModulesAssetsService] implemented auto caching files from atlases as… (#96)
    * [ModulesAssetsService] implemented auto caching files from atlases as textures

--- src/js/modules/assets/models/atlas.js
+++ src/js/modules/assets/models/atlas.js
@@ -4,6 +4,14 @@ class ModulesAssetsModelsAtlas extends Urso.Core.Modules.Assets.BaseModel {
 
         this.type = Urso.types.assets.ATLAS;
     }
+  
+    // cacheTextures: if true - add atlas parced textures in Urso textures cache with it's file name as asset key. 
+    // Example - atlas texture with path 'atlasPath/image/image_01.png' will be saved in cache with a key 'image_01'.
+    setupParams(params) {
+        super.setupParams(params);
+ 
+        this.cacheTextures = Urso.helper.recursiveGet('cacheTextures', params, false);
+    }
 }
 
 module.exports = ModulesAssetsModelsAtlas;
