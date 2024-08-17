4cc1243bcb87c2de935b1f10c093b376a6a7df21
megbrimef <megbrimef@gmail.com>
2024-08-17 21:15:54 +0300

    [Main] dropShadowFilter, inputText remove

--- src/js/extra/main.js
+++ src/js/extra/main.js
@@ -4,10 +4,6 @@ window.PIXI.particles = require('pixi-particles');
 
 window.PIXI.particlesFx = require('@urso/revolt-fx');
 
-import { DropShadowFilter } from '@pixi/filter-drop-shadow';
-PIXI.filters['DropShadowFilter'] = DropShadowFilter;
-
-
 window.PIXI.spine = require("pixi-spine");
 
 import { AtlasAttachmentLoader, SkeletonJson } from "@pixi-spine/runtime-3.8";
@@ -15,7 +11,6 @@ window.PIXI.spine.AtlasAttachmentLoader = AtlasAttachmentLoader;
 window.PIXI.spine.SkeletonJson = SkeletonJson;
 
 require("pixi-projection");
-require("pixi-text-input");
 
 import { gsap } from 'gsap';
 window.gsap = gsap;
