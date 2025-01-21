0c661ac91d12f9e60eef842c33aab3e612fcd996
demetriy92 <dmytriyshtepa@gmail.com>
2025-01-21 17:21:10 +0200

    [HitArea] added press check for hit area, changed handler for pointerUpOutiside, returned text input

--- src/js/modules/objects/models/hitArea.js
+++ src/js/modules/objects/models/hitArea.js
@@ -1,9 +1,11 @@
 const ModulesObjectsBaseModel = require('./../baseModel');
 
 class ModulesObjectsModelsHitArea extends ModulesObjectsBaseModel {
+
     constructor(params) {
         super(params);
         this._isDisabled = false;
+        this._isDown = false;
 
         this.type = Urso.types.objects.HITAREA;
 
@@ -26,6 +28,16 @@ class ModulesObjectsModelsHitArea extends ModulesObjectsBaseModel {
         this.onOutCallback = Urso.helper.recursiveGet('onOutCallback', params, false);
         this.onTouchMoveCallback = Urso.helper.recursiveGet('onTouchMoveCallback', params, false);
         this.handlePointerupoutside = Urso.helper.recursiveGet('handlePointerupoutside', params, true);
+        /**
+         * customInteractionArea object ex:
+         * Circle: { "type": "circle", "params": [0, 0, 100] }
+         * Rectangle: { "type": "rectangle", "params": [0, 0, 100, 100] }
+         * Polygon: {
+         *   "type": "polygon",
+         *   "params":  [ 0, 0, 100, 0, 100, 100, 50, 150, 0, 100 ];
+         * }
+         * */
+        this.customInteractionArea = Urso.helper.recursiveGet('customInteractionArea', params, null);
     }
 
     enable() {
@@ -58,14 +70,14 @@ class ModulesObjectsModelsHitArea extends ModulesObjectsBaseModel {
         this._baseObject
             .on('pointerdown', this._onPressDown.bind(this))
             .on('pointerup', this._onPressUp.bind(this))
+            .on('pointerupoutside', this._onPressUpOutside.bind(this))
             .on('pointerover', this._onOver.bind(this))
             .on('pointerout', this._onOut.bind(this))
             .on('touchmove', this._onTouchmove.bind(this));
 
-        if (this.handlePointerupoutside) {
-            this._baseObject.on('pointerupoutside', this._onPressUp.bind(this))
-        }
-    };
+        if (this.customInteractionArea)
+            this._baseObject.hitArea = this._getHitAreaObject(this.customInteractionArea);
+    }
 
     _onTouchmove(event) {
         const position = this._getEventLocalPosition(event);
@@ -81,6 +93,11 @@ class ModulesObjectsModelsHitArea extends ModulesObjectsBaseModel {
         if (this.disableRightClick && event.data.button !== 0)
             return;
 
+        if (this._isDown)
+            return false;
+
+        this._isDown = true;
+
         if (this.keyDownAction) {
             const position = this._getEventLocalPosition(event);
             this.keyDownAction(position);
@@ -94,10 +111,27 @@ class ModulesObjectsModelsHitArea extends ModulesObjectsBaseModel {
         if (this.disableRightClick && event.data.button !== 0)
             return;
 
+        if (!this._isDown)
+            return false;
+
+        this._isDown = false;
+
         if (this.action) {
             const position = this._getEventLocalPosition(event);
             this.action(position);
         }
+    }
+
+    _onPressUpOutside() {
+        if (this.handlePointerupoutside) {
+            this._onPressUp();
+            return;
+        }
+
+        if (this._isDisabled)
+            return false;
+
+        this._isDown = false;
     }
 
     _onOver() {
@@ -124,6 +158,24 @@ class ModulesObjectsModelsHitArea extends ModulesObjectsBaseModel {
         const y = event.data.global.y / worldScale.y;
 
         return { x, y };
+    }
+
+    _getHitAreaObject({ type, params }) {
+        if (!type) {
+            return null;
+        }
+
+        const objects = {
+            'rectangle': PIXI.Rectangle,
+            'circle': PIXI.Circle,
+            'polygon': PIXI.Polygon
+        }
+
+        if (!objects[type]) {
+            return null;
+        }
+
+        return new objects[type](...params);
     }
 }
 
