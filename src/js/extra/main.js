93e22cf5575cd2dce6509aeea59a66b8596eb7dd
demetriy92 <dmytriyshtepa@gmail.com>
2025-01-21 17:30:17 +0200

    removed text-input

--- src/js/extra/main.js
+++ src/js/extra/main.js
@@ -1,12 +1,10 @@
 import * as PIXI from 'pixi.js';
-import TextInput from 'pixi-text-input';
 window.PIXI = PIXI;
 window.PIXI.particles = require('pixi-particles');
 
 window.PIXI.particlesFx = require('@urso/revolt-fx');
 
 window.PIXI.spine = require("pixi-spine");
-window.PIXI.TextInput = TextInput;
 
 import { AtlasAttachmentLoader, SkeletonJson } from "@pixi-spine/runtime-3.8";
 window.PIXI.spine.AtlasAttachmentLoader = AtlasAttachmentLoader;
