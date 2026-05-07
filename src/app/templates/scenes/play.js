3e7f21bedf7b4a29175d7247e5d2a64260ade23d
alkott <alkott@hotmail.com>
2026-05-07 13:12:05 +0200

    feat: add play scene template with red square

---
+++ src/app/templates/scenes/play.js
@@ -0,0 +1,15 @@
+class AppTemplatesScenesPlay {
+    objects = [
+        {
+            type: Urso.types.objects.GRAPHICS,
+            x: 910,
+            y: 490,
+            figure: {
+                rectangle: [0, 0, 100, 100],
+                fillColor: 0xff0000
+            }
+        }
+    ];
+}
+
+export default AppTemplatesScenesPlay;
